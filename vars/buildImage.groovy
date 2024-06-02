a726c6bfc54ac40041db2f2431bcbf1d5ce7fc97
Eslam-Farg <loom2141@gmail.com>
2024-06-02 19:56:54 +0300

    adding params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 
-def call() {
+def call(string imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t eslam1/jenkins-repo:jma-2.0 .'
+        sh 'docker build -t $imageName .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push eslam1/jenkins-repo:jma-2.0'
+        sh 'docker push $imageName'
     }
 } 
